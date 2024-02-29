0ef13d4ce05e08905e996c9a9d63d78532bc6e88
vahid karimi <149614062+vahidKarimiDev@users.noreply.github.com>
2024-02-29 21:54:10 +0330

    add code js to dark mode and light

--- proje/js/app.js
+++ proje/js/app.js
@@ -1,8 +1,8 @@
 let $ = document;
 const navBtn = $.querySelector(".nav__btn");
 const navMenu = $.querySelector(".nav-menu");
-const likePost = $.querySelectorAll('.place_like')
-const menuItem = $.querySelectorAll('.menu-item')
+const likePost = $.querySelectorAll(".place_like");
+const menuItem = $.querySelectorAll(".menu-item");
 
 navBtn.addEventListener("click", () => {
   navBtn.classList.toggle("nav__btn--open");
@@ -20,23 +20,32 @@ toggleThemeBtn.addEventListener("click", () => {
   }
 });
 
-
+const toggleeThemeBtn = $.querySelector("#toggle-theme-1");
+toggleeThemeBtn.addEventListener("click", () => {
+  if (localStorage.theme === "dark") {
+    $.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+  } else {
+    $.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  }
+});
 
 likePost.forEach((item) => {
-    item.addEventListener('click' , () => {
-        let htmlColaction = item.children
-        let arrHtml = Array.from(htmlColaction)
+  item.addEventListener("click", () => {
+    let htmlColaction = item.children;
+    let arrHtml = Array.from(htmlColaction);
 
-        arrHtml.forEach((val) => {
-            val.classList.toggle('fill-[#E76F51]')
-        })
-    })
-})
+    arrHtml.forEach((val) => {
+      val.classList.toggle("fill-[#E76F51]");
+    });
+  });
+});
 
 menuItem.forEach((item) => {
-    item.addEventListener('click' , (e) => {
-        e.preventDefault()
-        $.querySelector('.menu-item__open').classList.remove('menu-item__open')
-        item.classList.add('menu-item__open')
-    })
-})
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    $.querySelector(".menu-item__open").classList.remove("menu-item__open");
+    item.classList.add("menu-item__open");
+  });
+});
